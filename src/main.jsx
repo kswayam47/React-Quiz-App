1c8efd0d8b40eb88fbe85aab44b978d27115c848
kswayam47 <swayamkewlani118@gmail.com>
2024-10-25 18:14:00 +0530

    Update paths for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-     <BrowserRouter>
+     <BrowserRouter basename="/React-Quiz-App">
     <App /> 
     </BrowserRouter>
 
